733bfbb87aa6b3f37301e65d5cd27e089d5c970f
Ananya Dixit <ananydixit8@gmail.com>
2021-05-02 19:32:06 +0530

    More functionality

--- js/cell.js
+++ js/cell.js
@@ -1,9 +1,12 @@
-function Cell(x, y, w) {
-    this.x = x;
-    this.y = y;
+function Cell(i, j, w) {
+    this.i = i;
+    this.j = j;
     this.w = w;
-    this.mine = true;
-    this.revealed = true;
+    this.x = i * w;
+    this.y = j * w;
+    this.mineCount = 0;
+    this.mine=false;
+    this.revealed = false;
 }
 
 Cell.prototype.show = function () {
@@ -11,5 +14,51 @@ Cell.prototype.show = function () {
     stroke(0);
     strokeWeight(2);
     noFill();
-    rect(this.x,this.y,this.w,this.w)
+    rect(this.x, this.y, this.w, this.w);
+    if (this.revealed) {
+        if (this.mine) {
+            fill(0);
+            ellipse(this.x + this.w * 0.5, this.y + this.w * 0.5, this.w * 0.5);
+        }
+        else {
+            stroke(0);
+            strokeWeight(2);
+            fill(200);
+            rect(this.x, this.y, this.w, this.w);
+            if(this.mineCount>0){
+                textAlign(CENTER);
+                fill(0);
+                strokeWeight(1);
+                text(this.mineCount, this.x + 0.5 * this.w, this.y + this.w -5);
+            }
+
+        }
+    }
+}
+
+Cell.prototype.countMines = function () {
+    if (this.mine) {
+        this.mineCount= -1;
+        return ;
+    }
+    var mines = 0;
+    for (var i = -1; i <= 1; i++) {
+        for (var j = -1; j <= 1; j++) {
+            var x = this.i + i;
+            var y = this.j + j;
+
+            if (x >= 0 && x < rows && y >= 0 && y < cols && grid[this.i + i][this.j + j].mine == true) {
+                mines++;
+            }
+        }
+    }
+    this.mineCount = mines;
+}
+
+Cell.prototype.contains = function (x, y) {
+    return (x > this.x && x < this.x + this.w && y > this.y && y < this.y + this.w);
+}
+
+Cell.prototype.reveal = function () {
+    this.revealed = true;
 }
--- js/game.js
+++ js/game.js
@@ -10,24 +10,106 @@ function makeGrid(rows, cols) {
 
 var grid;
 var w = 20;
-var rows = Math.floor(400 / w);
-var cols = Math.floor(400 / w);
+var rows = Math.floor(300 / w);
+var cols = Math.floor(300 / w);
+var totalMines = 30;
+
+
 
 function setup() {
-    createCanvas(400, 400);
+    createCanvas(300, 300);
 
     grid = makeGrid(rows, cols);
     for (var i = 0; i < rows; i++) {
         for (var j = 0; j < cols; j++) {
-            grid[i][j] = new Cell(i * w, j * w, w);
+            grid[i][j] = new Cell(i, j, w);
 
+        }
+    }
+
+    //Pick random mines 
+    var options = [];
+    for (var i = 0; i < rows; i++) {
+        for (var j = 0; j < cols; j++) {
+            options.push([i, j]);
+
+        }
+    }
+    for (var n = 0; n < totalMines; n++) {
+        var pick = floor(random(options.length));
+        var i = options[pick][0];
+        var j = options[pick][1];
+        options.splice(pick, 1);
+        grid[i][j].mine = true;
+    }
+
+    for (var i = 0; i < rows; i++) {
+        for (var j = 0; j < cols; j++) {
+            grid[i][j].countMines();
+
+        }
+    }
+}
+
+function dfs(i, j) {
+    if (grid[i][j].revealed == true || grid[i][j].mine == true) {
+        return;
+    }
+    grid[i][j].reveal();
+    if (grid[i][j].mineCount == 0) {
+        for (var h = -1; h <= 1; h++) {
+            for (var v = -1; v <= 1; v++) {
+                if (h == 0 && v == 0) {
+                    continue;
+                }
+                var x = h + i;
+                var y = v + j;
+
+                if (x >= 0 && x < rows && y >= 0 && y < cols) {
+                    dfs(x, y);
+                }
+            }
+        }
+    }
+    return;
+}
+
+
+function gameOver() {
+    for (var i = 0; i < rows; i++) {
+        for (var j = 0; j < cols; j++) {
+            if (grid[i][j].mine == true) {
+                grid[i][j].revealed = true;
+            }
+        }
+    }
+    var div = document.getElementById("ending");
+    div.innerHTML = "GAME OVER!";
+    return;
+
+
+}
+
+
+function mousePressed() {
+    for (var i = 0; i < rows; i++) {
+        for (var j = 0; j < cols; j++) {
+            if (grid[i][j].contains(mouseX, mouseY)) {
+                if (grid[i][j].mineCount == 0) {
+
+                    dfs(i, j);
+                }
+                grid[i][j].reveal();
+                if (grid[i][j].mine == true) {
+                    gameOver();
+                }
+            }
         }
     }
 }
 
 function draw() {
     clear();
-    
     for (var i = 0; i < rows; i++) {
         for (var j = 0; j < cols; j++) {
             grid[i][j].show();
